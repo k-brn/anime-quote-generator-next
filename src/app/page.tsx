1a9773b1f1758e3642d335e0d7e74263a9c408f3
k-brn <108495284+k-brn@users.noreply.github.com>
2023-07-26 13:58:29 -0700

    Update the page displaying quotes from the anime API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,40 @@
 "use client";
-
-import { useState, FormEvent } from "react";
 import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
 export default function Home() {
-  const [inputVal, setInputVal] = useState("");
+  const [value, setValue] = useState("");
   const { push } = useRouter();
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    push(`/qoute/${inputVal}`);
+    push(`/qoute/${value}`);
   };
   return (
-    <div>
-      <div>
-        <h1>🌌 Unleash Anime Inspiration With A Tap! 🎯</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="p-4 shadow-md bg-white rounded-md">
+        <h1 className="text-2xl font-semibold mb-4 text-black">
+          🔮 Your gateway to anime wisdom awaits! 🎯
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-3">
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded text-black"
+            placeholder="Enter anime name (e.g. Attack on Titan, Tokyo Ghoul, Berserk)"
+          />
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          >
+            Submit
+          </button>
+        </form>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter anime name..."
-          value={inputVal}
-          className="text-black"
-          onChange={(e) => setInputVal(e.target.value)}
-        />
-        <button type="submit">Get Quote</button>
-      </form>
-      <p>Created by Korrier with ❤️</p>
+      <footer className="text-center text-gray-500 mt-4">
+        Created by Korrier with ❤️
+      </footer>
     </div>
   );
 }
--- src/app/qoute/[name]/page.tsx
+++ src/app/qoute/[name]/page.tsx
@@ -1,12 +1,33 @@
+const getQoute = async (name: string) => {
+  const res = await fetch(
+    `https://animechan.xyz/api/random/anime?title=${name}`
+  );
+  return res.json();
+};
+
 interface Params {
   params: { name: string };
 }
 
-export default function Page({ params }: Params) {
+async function Quote({ params }: Params) {
+  const quoteData = await getQoute(params.name);
+  const anime = quoteData?.anime;
+  const quote = quoteData?.quote;
+  const character = quoteData?.character;
+
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      {" "}
-      {params.name}
-    </main>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="w-[544px] p-4 shadow-md bg-white rounded-md">
+        <p className="text-black md:text-xl my-4">
+          <span className="font-bold">"{quote}"</span>
+          <br />
+          <span className="block text-center">
+            - {character} ({anime})
+          </span>
+        </p>
+      </div>
+    </div>
   );
 }
+
+export default Quote;
